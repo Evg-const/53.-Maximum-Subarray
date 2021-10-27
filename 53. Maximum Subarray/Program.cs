0c53f837fbc0b33727a06c496f322feab8175b7e
Евгений Константинов <kanstantsinav@gmail.com>
2021-10-27 21:39:13 +0300

    Добавьте файлы проекта.

---
+++ 53. Maximum Subarray/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _53.Maximum_Subarray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = { -2, -1, -3, 4, -1, 2, 1, -5, 4 };
+
+            Console.WriteLine(Convert.ToString(MaxSubArray(input)));
+
+            Console.Read();
+
+
+            int MaxSubArray(int[] nums)
+            {
+                int temp =  nums[0];
+                int ans = nums[0];
+
+                for (int i = 0; i < nums.Length; i++)
+                {                                      
+                    temp = Math.Max(nums[i], (temp + nums[i]));
+                    ans = Math.Max(temp, ans);
+
+                }
+                return ans;
+            }
+
+
+
+
+        }
+    }
+}
